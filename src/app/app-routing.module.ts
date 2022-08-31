c039488f0c95e6c410d66985c968e0f71c044334
acevedoseba <sacevedog93@gmail.com>
2022-08-31 19:29:23 -0400

    clase 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,27 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'inicio',
-    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
-  },
-  {
-    path: 'perfil',
-    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
-  },
-  {
-    path: 'menu-tabs',
-    loadChildren: () => import('./menu-tabs/menu-tabs.module').then( m => m.MenuTabsPageModule)
-  },
+    loadChildren: () => import('./tablinks/tablinks.module').then( m => m.TablinksPageModule)
+  }
 ];
 
 @NgModule({
